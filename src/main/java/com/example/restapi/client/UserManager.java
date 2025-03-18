7607aef5da248fcd01a2fa544ba3ee677fd388cb
Victor Redondo Mula <victor.redondo@opendeusto.es>
2025-03-18 21:54:49 +0100

    HTML + Templates + Controller
    Hay que hacer un seguimiento de los urls en el controller del cliente e ir actualizando tras cada petición.

--- src/main/java/com/example/restapi/client/UserManager.java
+++ src/main/java/com/example/restapi/client/UserManager.java
@@ -1,8 +1,11 @@
 package com.example.restapi.client;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +13,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.restapi.model.User;
 import jakarta.servlet.http.HttpServletRequest;
-
+@Controller
 public class UserManager {
 
 	private String USER_CONTROLLER_URL_TEMPLATE = "http://%s:%s/auth/registration";
@@ -23,17 +26,23 @@ public class UserManager {
 		USER_CONTROLLER_URL = String.format(USER_CONTROLLER_URL_TEMPLATE, hostname, port);
 		this.restTemplate = new RestTemplate();
 	}
+
+	public UserManager() {
+        USER_CONTROLLER_URL = "http://localhost:8080/auth/registration";
+        this.restTemplate = new RestTemplate();
+	}
 	
-//	@ModelAttribute
-//	public void addAttributes(Model model, HttpServletRequest request) {
-//		model.addAttribute("currentUrl", ServletUriComponentsBuilder.fromRequestUri(request).toUriString());
-//		model.addAttribute("responseRegister", responseRegister);
-//	}
+	@ModelAttribute
+	public void addAttributes(Model model, HttpServletRequest request) {
+		model.addAttribute("currentUrl", ServletUriComponentsBuilder.fromRequestUri(request).toUriString());
+	}
 	
-	@PostMapping("/auth/registration")
-	public String registerUser(@RequestParam String username, @RequestParam String password, 
-			@RequestParam String name, @RequestParam String surnames, @RequestParam int age, Model model) {
-		
+	
+	
+	@PostMapping("/registration")
+	public String registerUser(@RequestParam String username,@RequestParam String password,@RequestParam String name,
+			@RequestParam String surnames,@RequestParam int age, Model model) {
+		System.out.println("User: " + username + " " + password + " " + name + " " + surnames + " " + age);
 		User user = new User(username, password, name, surnames, age);
 		
 		if (registerUser(user)) {
@@ -46,13 +55,19 @@ public class UserManager {
 	}
 	
 	public boolean registerUser(User user) {
-		ResponseEntity<Void> userResponse = restTemplate.postForEntity(USER_CONTROLLER_URL, user, Void.class);
-		
+		System.out.println("User: " + user.getUsername() + " " + user.getPassword() + " " + user.getName() + " " + user.getSurnames() + " " + user.getAge());
+		ResponseEntity<User> userResponse = restTemplate.postForEntity(USER_CONTROLLER_URL, user, User.class);
+		System.out.println(userResponse.getStatusCode());
 		if (userResponse.getStatusCode().is2xxSuccessful()) {
 			return true;
 		} else {
 			return false;
 		}
 	}
+
+	 @GetMapping("/home")
+	    public String registerForm(User user) {
+	        return "registration"; 
+	    }
 	
 }
