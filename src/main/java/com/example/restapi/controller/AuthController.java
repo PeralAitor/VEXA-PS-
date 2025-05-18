985bd5804e4ef944c96e782ca109293718ff9308
Victor Redondo Mula <victor.redondo@opendeusto.es>
2025-05-18 16:26:37 +0200

    Documentación AuthController

--- src/main/java/com/example/restapi/controller/AuthController.java
+++ src/main/java/com/example/restapi/controller/AuthController.java
@@ -19,21 +19,32 @@ import com.example.restapi.service.AuthService;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-	
+
 	private final AuthService authService;
-	
+
 	public AuthController(AuthService authService) {
 		this.authService = authService;
 	}
-	
+	/**
+	 * * Endpoint para registrar un nuevo usuario.
+	 * @param userDTO Claves para el registro de un usuario, incluye username, password, edad,etc...
+	 * @return
+	 */
 	@PostMapping("/registration")
 	public ResponseEntity<User> register(@RequestBody UserDTO userDTO) {
 		if (authService.register(userDTO)) {
 			return new ResponseEntity<>(HttpStatus.OK);
-        }
+		}
 		return new ResponseEntity<>(HttpStatus.CONFLICT);
-    }
-	
+	}
+
+	/**
+	 * Endpoint para iniciar sesión.
+	 * 
+	 * @param credentialsDTO Claves para el login de un usuario, incluye username y
+	 *                       password
+	 * @return
+	 */
 	@PostMapping("/login")
 	public ResponseEntity<String> login(@RequestBody CredentialsDTO credentialsDTO) {
 		String response = authService.login(credentialsDTO);
@@ -42,7 +53,13 @@ public class AuthController {
 		}
 		return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
 	}
-	
+
+	/**
+	 * Endpoint para cerrar sesión.
+	 * 
+	 * @param token Clave de sesión del usuario
+	 * @return
+	 */
 	@PostMapping("/logout")
 	public ResponseEntity<Boolean> logout(@RequestBody String token) {
 		if(authService.logout(token)) {
@@ -50,16 +67,22 @@ public class AuthController {
 		}
 		return new ResponseEntity<>(false, HttpStatus.UNAUTHORIZED);
 	}
-	
+
+	/**
+	 * Endpoint para obtener todos los usuarios.
+	 * 
+	 * @param token Clave de sesión del usuario
+	 * @return
+	 */
 	@GetMapping("/users")
-    public ResponseEntity<List<User>> getAllUsers(@RequestParam String token) {
-        // Verificar si es admin
-        User user = AuthService.getUserFromMap(token);
-        if (user != null && "admin".equals(user.getUsername())) {
-            return ResponseEntity.ok(authService.getAllUsers(token));
-        }
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-    }
+	public ResponseEntity<List<User>> getAllUsers(@RequestParam String token) {
+		// Verificar si es admin
+		User user = AuthService.getUserFromMap(token);
+		if (user != null && "admin".equals(user.getUsername())) {
+			return ResponseEntity.ok(authService.getAllUsers(token));
+		}
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+	}
 }
 
 
